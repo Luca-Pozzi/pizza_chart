65a41058f48a3c590c5a4d6828786e581a5596c7
Luca <luca6.pozzi@mail.polimi.it>
2025-01-30 15:03:18 +0100

    move pizza_chart function to dedicated script

---
+++ pizza_chart.py
@@ -0,0 +1,74 @@
+# Adapted from: https://stackoverflow.com/questions/44528024/insert-image-into-pie-chart-slice/44529673#44529673
+
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+from matplotlib.patches import PathPatch
+from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+
+def plot_pizza_chart(order, title=None, cutoff=7.0, show=True, wedgeprops={},
+                    textprops={}):
+    labels = []
+    data = []
+    stats = []
+    tot_num = sum([v for __, v in order])
+    others = 0
+    for pizza, value in order:
+        if value/tot_num * 100 >= cutoff:
+            labels.append(pizza)
+            data.append(value)
+            stats.append('{:.2f}% ({})'.format(value/tot_num*100, 
+                                            value)
+                        )
+        else:
+            others += value
+    if others: # if is any pizza in `other` category, save it here
+        labels.append('altro')
+        data.append(others)
+        stats.append('{:.2f}% ({})'.format(others/tot_num*100, 
+                                           others)
+                        )
+
+    # Create the pie chart
+    fig, ax = plt.subplots()
+    if title:
+        plt.title(title)
+    plt.gca().axis("equal")
+    wedges, __ = plt.pie(data, startangle=90, 
+                         labels=[lab+'\n'+stat for lab, stat in zip(labels, stats)],
+                         explode = [0.1] * len(labels),
+                         wedgeprops = { 'linewidth': 2, 
+                                        "edgecolor" :'w',
+                                        "fill":False,
+                                        **wedgeprops
+                                        },
+                         textprops={'color':"grey",
+                                    **textprops
+                                    }
+                            )
+    # Display pizza textures in chart sectors
+    for i in range(len(labels)):
+        try:
+            fn = os.path.join("assets",
+                            "pizzas",
+                            "{}.png".format(labels[i].lower().replace(" ", "_"))
+                            )
+            _img_to_pie(fn, wedges[i], xy=(0.0, 0.0), zoom=0.45)
+            wedges[i].set_zorder(10)
+        except FileNotFoundError:
+            continue
+    if show:
+        # Plot the figure
+        plt.show()
+    return fig
+
+def _img_to_pie( fn, wedge, xy, zoom=1, ax = None):
+    # Fill pie chart sectors with images
+    if ax==None: ax=plt.gca()
+    im = plt.imread(fn, format='png')
+    path = wedge.get_path()
+    patch = PathPatch(path, facecolor='none')
+    ax.add_patch(patch)
+    imagebox = OffsetImage(im, zoom=zoom, clip_path=patch, zorder=-10)
+    ab = AnnotationBbox(imagebox, xy, xycoords='data', pad=0, frameon=False)
+    ax.add_artist(ab)
